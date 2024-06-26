aa40ea7dfb727513ac085ec56e66dbf6e391db33
LuciaRF <7lucilda+2@gmail.com>
2024-06-26 15:24:59 +0200

    Primer commit

---
+++ APIS3D_2023/APIS3D_2023.cpp
@@ -0,0 +1,15 @@
+// APIS3D_2023.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include "common.h"
+#include <math.h>
+
+
+
+int main(int argc, char** argv)
+{
+
+    std::cout << "Hello World!\n";
+    return 0;
+}
